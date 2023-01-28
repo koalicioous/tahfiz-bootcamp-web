b10cba4cabce4425d939864ddb2eaa54a9cc7025
Surya Negara <surya.negara@gontor.ac.id>
2023-01-28 13:14:08 +0700

    setup: add tailwind and headless ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import CalculatorPlayground from "@/components/Playground/Calculator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,26 +16,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <div className={`${styles.center} ${inter.className}`}>
+          <CalculatorPlayground />
         </div>
       </main>
     </>
   );
 }
+
+/**
+ * Input:
+ * 1. Pilih berapa banyak yang ingin dihafal
+ * 2. Pilih target waktu
+ *
+ * Output:
+ * 1. Jumlah ayat yang harus dihafal per hari
+ *
+ */
